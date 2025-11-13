26b63ffac8d958992f5d2476e75a4069a323cf32
YuriVasconcelosj <yurivasconcelosavatar@gmail.com>
2025-11-13 18:25:56 -0300

    feat: Criação da classe FormtUtils para armazenar os métodos de formatação.

---
+++ mercadinho-providence/src/main/java/br/com/mercadinhoprovidence/util/FormatUtils.java
@@ -0,0 +1,52 @@
+package br.com.mercadinhoprovidence.util;
+
+import java.math.BigDecimal;
+import java.text.NumberFormat;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Locale;
+
+public class FormatUtils {
+
+    private static final DateTimeFormatter dateFormatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+    private static final NumberFormat currencyFormat = NumberFormat.getCurrencyInstance(Locale.of("pt", "BR"));
+
+    /**
+     * Formata um booleano para "Sim" ou "Não".
+     *
+     * @param value Valor booleano.
+     * @return "Sim" se true, "Não" se false.
+     */
+    public static String formatBoolean(Boolean value) {
+        if (value == null) {
+            return "Não informado";
+        }
+        return value ? "Sim" : "Não";
+    }
+
+    /**
+     * Formata um LocalDate para String no formato "dd/MM/yyyy".
+     *
+     * @param date Data a ser formatada.
+     * @return String formatada da data.
+     */
+    public static String formatDate(LocalDate date) {
+        if (date == null) {
+            return null;
+        }
+        return date.format(dateFormatter);
+    }
+
+    /**
+     * Formata um valor BigDecimal como moeda brasileira (R$).
+     *
+     * @param unformattedSalary Salário sem formatação.
+     * @return Retorna o salário formatado como moeda.
+     */
+    public static String formatCurrency(BigDecimal unformattedSalary) {
+        if (unformattedSalary == null) {
+            return currencyFormat.format(BigDecimal.ZERO);
+        }
+        return currencyFormat.format(unformattedSalary);
+    }
+}
