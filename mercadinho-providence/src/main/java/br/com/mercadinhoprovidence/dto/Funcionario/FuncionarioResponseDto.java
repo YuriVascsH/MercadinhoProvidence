a630fcbfe3de0493e4ccc005d179163320780ce8
YuriVasconcelosj <yurivasconcelosavatar@gmail.com>
2025-11-14 18:49:33 -0300

    Feat: Adiciona a classe FuncionarioResponseDto para ser retornado para CadastroFuncionarioDialog após a criação de um funcionario no banco de dados

---
+++ mercadinho-providence/src/main/java/br/com/mercadinhoprovidence/dto/Funcionario/FuncionarioResponseDto.java
@@ -0,0 +1,17 @@
+package br.com.mercadinhoprovidence.dto.Funcionario;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class FuncionarioResponseDto {
+
+    private String nome;
+    private Integer codigoVerificador;
+
+}
