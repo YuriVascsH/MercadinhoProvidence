b3e8024142f78eb983fe4e9affc31b9e967c77d9
YuriVasconcelosj <yurivasconcelosavatar@gmail.com>
2025-11-14 18:53:15 -0300

    Feat: Adiciona o método verificarCpfExistente na classe FuncionarioService

--- mercadinho-providence/src/main/java/br/com/mercadinhoprovidence/Service/FuncionarioService.java
+++ mercadinho-providence/src/main/java/br/com/mercadinhoprovidence/Service/FuncionarioService.java
@@ -1,6 +1,8 @@
 package br.com.mercadinhoprovidence.Service;
 
 import br.com.mercadinhoprovidence.dao.FuncionarioDao;
+import br.com.mercadinhoprovidence.dto.Funcionario.FuncionarioCreateDto;
+import br.com.mercadinhoprovidence.dto.Funcionario.FuncionarioResponseDto;
 import br.com.mercadinhoprovidence.dto.FuncionarioTableDto;
 import br.com.mercadinhoprovidence.model.Funcionario;
 
@@ -15,13 +17,37 @@ public class FuncionarioService {
         if (funcionario == null) {
             return null;
         }
-        FuncionarioTableDto funcionarioTableDto = new FuncionarioTableDto(funcionario);
-        return funcionarioTableDto;
+        return new FuncionarioTableDto(funcionario);
     }
 
     public List<FuncionarioTableDto> buscarTodosOsFuncionarios() {
         List<Funcionario> funcionarios = funcionarioDao.listarTodos();
         return funcionarios.stream().map(funcionario -> new FuncionarioTableDto()).toList();
+    }
+
+    public boolean verificarCpfExistente(String cpf) {
+        return funcionarioDao.verificarCpfExistente(cpf);
+    }
+
+    public FuncionarioResponseDto salvarFuncionario(FuncionarioCreateDto funcionarioCreateDto) {
+        Funcionario funcionario = new Funcionario();
+        funcionario.setCodigoVerificador(funcionarioCreateDto.getCodigoVerificador());
+        funcionario.setCpf(funcionarioCreateDto.getCpf());
+        funcionario.setNome(funcionarioCreateDto.getNome());
+        funcionario.setDataNascimento(funcionarioCreateDto.getDataNascimento());
+        funcionario.setTelefone(funcionarioCreateDto.getTelefone());
+        funcionario.setEmail(funcionarioCreateDto.getEmail());
+        funcionario.setEndereco(funcionarioCreateDto.getEndereco());
+        funcionario.setDataAdmissao(funcionarioCreateDto.getDataAdmissao());
+        funcionario.setCargo(funcionarioCreateDto.getCargo());
+        funcionario.setSalario(funcionarioCreateDto.getSalario());
+        funcionario.setSenha(funcionarioCreateDto.getSenha());
+        funcionario.setAtivo(funcionarioCreateDto.getAtivo());
+        funcionario.setUltimaVenda(funcionarioCreateDto.getUltimaVenda());
+
+        Funcionario funcionarioSalvo = funcionarioDao.inserir(funcionario);
+
+        return new FuncionarioResponseDto(funcionarioSalvo.getNome(), funcionarioSalvo.getCodigoVerificador());
     }
 
     public Boolean deletarFuncionario(Integer idFuncionario) {
