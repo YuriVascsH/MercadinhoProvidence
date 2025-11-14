982aed94e3a93678432dbfc498378c37d8a62068
YuriVasconcelosj <yurivasconcelosavatar@gmail.com>
2025-11-14 02:20:29 -0300

    feat: Criação da classe FuncionarioService para a realização do mapeamento da classe funcionario e conexão com a classe FuncionarioDao.

--- mercadinho-providence/src/main/java/br/com/mercadinhoprovidence/Service/FuncionarioService.java
+++ mercadinho-providence/src/main/java/br/com/mercadinhoprovidence/Service/FuncionarioService.java
@@ -1,9 +1,32 @@
 package br.com.mercadinhoprovidence.Service;
 
 import br.com.mercadinhoprovidence.dao.FuncionarioDao;
+import br.com.mercadinhoprovidence.dto.FuncionarioTableDto;
 import br.com.mercadinhoprovidence.model.Funcionario;
 
+import java.util.List;
+
 public class FuncionarioService {
+
+    private final FuncionarioDao funcionarioDao = new FuncionarioDao();
+
+    public FuncionarioTableDto buscarPorCpf(String cpf) {
+        Funcionario funcionario = funcionarioDao.buscarPorCpf(cpf);
+        if (funcionario == null) {
+            return null;
+        }
+        FuncionarioTableDto funcionarioTableDto = new FuncionarioTableDto(funcionario);
+        return funcionarioTableDto;
+    }
+
+    public List<FuncionarioTableDto> buscarTodosOsFuncionarios() {
+        List<Funcionario> funcionarios = funcionarioDao.listarTodos();
+        return funcionarios.stream().map(funcionario -> new FuncionarioTableDto()).toList();
+    }
+
+    public Boolean deletarFuncionario(Integer idFuncionario) {
+        return funcionarioDao.deletar(idFuncionario);
+    }
 
 
 }
