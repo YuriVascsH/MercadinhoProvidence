9df828da7d10cc1deb7aba2fe4932d2787b3796a
YuriVasconcelosj <yurivasconcelosavatar@gmail.com>
2025-11-14 18:45:40 -0300

    Feat: Adiciona os métodos salvarFuncionario e verificarCpfExistente na classe FuncionarioController

--- mercadinho-providence/src/main/java/br/com/mercadinhoprovidence/controller/FuncionarioController.java
+++ mercadinho-providence/src/main/java/br/com/mercadinhoprovidence/controller/FuncionarioController.java
@@ -1,23 +1,80 @@
 package br.com.mercadinhoprovidence.controller;
 
 import br.com.mercadinhoprovidence.Service.FuncionarioService;
+import br.com.mercadinhoprovidence.dto.Funcionario.FuncionarioCreateDto;
+import br.com.mercadinhoprovidence.dto.Funcionario.FuncionarioResponseDto;
 import br.com.mercadinhoprovidence.dto.FuncionarioTableDto;
 
+import java.math.BigDecimal;
+import java.time.LocalDate;
 import java.util.List;
 
 public class FuncionarioController {
 
     FuncionarioService funcionarioService = new FuncionarioService();
 
+    /**
+     * Função que busca um funcionario no banco a partir do cpf informado e retorana um FuncionarioTableDto
+     *
+     * @param cpf informado pelo user na barra de pesquisa
+     *
+     * @return o funcionarioTableDto contendo as informações cadastradas no banco
+     * */
     public FuncionarioTableDto buscarPorCpf(String cpf) {
         return funcionarioService.buscarPorCpf(cpf);
     }
 
+    /**
+     * Função que buscca no banco retorna uma lista de funcionariosTableDto para popular a tabela da view
+     *
+     * @return uma lista contendo FuncionarioTableDto
+     * */
     public List<FuncionarioTableDto> listarTodos() {
         return funcionarioService.buscarTodosOsFuncionarios();
     }
 
+    /**
+     * Função que retorna um valor booleano para deletar um funcionario do banco de dados
+     * REVISAR
+     * @param idFuncionario insreido ao ser clicado no botão de deletar
+     *
+     * @return valor booleano para disparar o alerta de sucesso
+     * */
     public Boolean deletarFuncionario(Integer idFuncionario) {
         return funcionarioService.deletarFuncionario(idFuncionario);
     }
+
+    /**
+     * Função que retorna um FuncionarioResponseDto para a view, responsável pela verificação dos campos null dos atriubytos
+     * Data de nascimento, Data de admissão e salário.
+     *
+     * @param funcionarioCreateDto inforamdo pelo user contendo os dados do funcionario a ser salvo no banco de dados
+     *
+     * @return funcionarioResponseDto contendo as informações de criação do funcionário
+     */
+    public FuncionarioResponseDto salvarFuncionario(FuncionarioCreateDto funcionarioCreateDto) {
+        if (funcionarioCreateDto.getDataAdmissao() == null) {
+            funcionarioCreateDto.setDataAdmissao(LocalDate.now());
+        }
+        if(funcionarioCreateDto.getDataNascimento() == null) {
+            funcionarioCreateDto.setDataNascimento(LocalDate.of(2000, 1, 1));
+        }
+        if (funcionarioCreateDto.getSalario() == null) {
+            funcionarioCreateDto.setSalario(BigDecimal.ZERO);
+        }
+        return funcionarioService.salvarFuncionario(funcionarioCreateDto);
+    }
+
+    /**
+     * Funação que retorna um valor booleano para verificar se já existe um Funcionário cadastrado com o cpf informado
+     * pelo user
+     *
+     * @param cpf valor informado pelo usuario para a verificação do cpf
+     *
+     * @return valor verdadeiro ou falso de acordo com o resultado da pesquisa
+     */
+    public boolean verificarCpfExistente(String cpf) {
+        return funcionarioService.verificarCpfExistente(cpf);
+    }
+
 }
