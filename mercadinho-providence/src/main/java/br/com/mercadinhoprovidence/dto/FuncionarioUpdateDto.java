83ef1830b98565a8a8d6f210e3412b019618c4e2
YuriVasconcelosj <yurivasconcelosavatar@gmail.com>
2025-11-22 21:12:55 -0300

    feat: Adiciona o FuncionarioUpdateDto para a realização das atualizações de funcionario

---
+++ mercadinho-providence/src/main/java/br/com/mercadinhoprovidence/dto/FuncionarioUpdateDto.java
@@ -0,0 +1,24 @@
+package br.com.mercadinhoprovidence.dto;
+
+import br.com.mercadinhoprovidence.model.enums.Cargo;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class FuncionarioUpdateDto {
+
+    private Integer idFuncionario;
+    private String nome;
+    private String telefone;
+    private String email;
+    private String endereco;
+    private Cargo cargo;
+    private BigDecimal salario;
+    private Boolean ativo;
+
+}
