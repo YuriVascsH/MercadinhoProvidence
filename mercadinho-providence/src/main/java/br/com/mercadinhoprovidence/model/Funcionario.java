a774a5202fb8624a6faeb78bfda147e428cc6322
Felipe Sabino de Oliveira <fsabino006@gmail.com>
2025-11-25 11:48:31 -0300

    add: getters e setters de Funcionario (model)

--- mercadinho-providence/src/main/java/br/com/mercadinhoprovidence/model/Funcionario.java
+++ mercadinho-providence/src/main/java/br/com/mercadinhoprovidence/model/Funcionario.java
@@ -30,5 +30,90 @@ public class Funcionario {
 	private String senha;
 	private Boolean ativo;
     private LocalDateTime ultimaVenda;
-
+	public Integer getIdFuncionario() {
+		return idFuncionario;
+	}
+	public void setIdFuncionario(Integer idFuncionario) {
+		this.idFuncionario = idFuncionario;
+	}
+	public Integer getCodigoVerificador() {
+		return codigoVerificador;
+	}
+	public void setCodigoVerificador(Integer codigoVerificador) {
+		this.codigoVerificador = codigoVerificador;
+	}
+	public String getCpf() {
+		return cpf;
+	}
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public LocalDate getDataNascimento() {
+		return dataNascimento;
+	}
+	public void setDataNascimento(LocalDate dataNascimento) {
+		this.dataNascimento = dataNascimento;
+	}
+	public String getTelefone() {
+		return telefone;
+	}
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getEndereco() {
+		return endereco;
+	}
+	public void setEndereco(String endereco) {
+		this.endereco = endereco;
+	}
+	public LocalDate getDataAdmissao() {
+		return dataAdmissao;
+	}
+	public void setDataAdmissao(LocalDate dataAdmissao) {
+		this.dataAdmissao = dataAdmissao;
+	}
+	public Cargo getCargo() {
+		return cargo;
+	}
+	public void setCargo(Cargo cargo) {
+		this.cargo = cargo;
+	}
+	public BigDecimal getSalario() {
+		return salario;
+	}
+	public void setSalario(BigDecimal salario) {
+		this.salario = salario;
+	}
+	public String getSenha() {
+		return senha;
+	}
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+	public Boolean getAtivo() {
+		return ativo;
+	}
+	public void setAtivo(Boolean ativo) {
+		this.ativo = ativo;
+	}
+	public LocalDateTime getUltimaVenda() {
+		return ultimaVenda;
+	}
+	public void setUltimaVenda(LocalDateTime ultimaVenda) {
+		this.ultimaVenda = ultimaVenda;
+	}
+    
+    
 }
