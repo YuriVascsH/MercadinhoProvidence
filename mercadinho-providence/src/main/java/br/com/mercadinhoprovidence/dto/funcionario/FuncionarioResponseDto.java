e0e1bb35a6e675ea97606adbfc6ad0de9f00de75
YuriVasconcelosj <yurivasconcelosavatar@gmail.com>
2025-12-27 13:21:30 -0300

    feat: Adiciona os dto de funcionários

---
+++ mercadinho-providence/src/main/java/br/com/mercadinhoprovidence/dto/funcionario/FuncionarioResponseDto.java
@@ -0,0 +1,23 @@
+package br.com.mercadinhoprovidence.dto.funcionario;
+
+import br.com.mercadinhoprovidence.model.Funcionario;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class FuncionarioResponseDto {
+
+    private String nome;
+    private Integer codigoVerificador;
+
+    public FuncionarioResponseDto(Funcionario funcionario) {
+        this.nome = funcionario.getNome();
+        this.codigoVerificador = funcionario.getCodigoVerificador();
+    }
+
+}
---
+++ mercadinho-providence/src/main/java/br/com/mercadinhoprovidence/dto/funcionario/FuncionarioTableDto.java
@@ -0,0 +1,38 @@
+package br.com.mercadinhoprovidence.dto.funcionario;
+
+import br.com.mercadinhoprovidence.model.Funcionario;
+import br.com.mercadinhoprovidence.model.enums.Cargo;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class FuncionarioTableDto {
+
+    private Integer idFuncionario;
+    private Integer codigoFuncionario;
+    private String nome;
+    private String cpf;
+    private Cargo cargo;
+    private BigDecimal salario;
+    private LocalDate dataAdmissao;
+    private Boolean ativo;
+
+    public FuncionarioTableDto(Funcionario funcionario) {
+        this.idFuncionario = funcionario.getIdFuncionario();
+        this.codigoFuncionario = funcionario.getCodigoVerificador();
+        this.nome = funcionario.getNome();
+        this.cpf = funcionario.getCpf();
+        this.cargo = funcionario.getCargo();
+        this.salario = funcionario.getSalario();
+        this.dataAdmissao = funcionario.getDataAdmissao();
+        this.ativo = funcionario.getAtivo();
+    }
+}
