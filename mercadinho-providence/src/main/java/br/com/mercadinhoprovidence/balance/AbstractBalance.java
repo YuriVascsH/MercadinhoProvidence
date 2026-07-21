6069883008f0c30c2a5c6532eb5e994db04a4d7b
Felipe Sabino de Oliveira Lima <fsabino006@gmail.com>
2026-07-21 08:35:05 -0300

    refactor: Config and Abstract Classes are better elaboreted.

--- mercadinho-providence/src/main/java/br/com/mercadinhoprovidence/balance/AbstractBalance.java
+++ mercadinho-providence/src/main/java/br/com/mercadinhoprovidence/balance/AbstractBalance.java
@@ -3,10 +3,25 @@ package br.com.mercadinhoprovidence.balance;
 import br.com.mercadinhoprovidence.balance.config.BalanceConfig;
 import br.com.mercadinhoprovidence.balance.serial.SerialConnection;
 
+package br.com.mercadinhoprovidence.balanca;
+
+import br.com.mercadinhoprovidence.balanca.serial.SerialConnection;
+
 public abstract class AbstractBalance implements Balance {
 
-    protected BalanceConfig config;
+    protected final BalanceConfig config;
 
     protected SerialConnection serialConnection;
+
+    protected boolean connected;
+
+    protected AbstractBalance(BalanceConfig config) {
+        this.config = config;
+    }
+
+    @Override
+    public boolean isConnected() {
+        return connected;
+    }
 
 }
--- mercadinho-providence/src/main/java/br/com/mercadinhoprovidence/balance/config/BalanceConfig.java
+++ mercadinho-providence/src/main/java/br/com/mercadinhoprovidence/balance/config/BalanceConfig.java
@@ -1,15 +1,18 @@
 package br.com.mercadinhoprovidence.balance.config;
 
+@Getter
+@Setter
+@NoArgsConstructor
 public class BalanceConfig {
 
-    private String porta; // COM4 (provavelmente)
+    private String portName = "COM4";
 
-    private int baudRate; // 9600
+    private int baudRate = 9600;
 
-    private int dataBits; // 8
+    private int dataBits = 8;
 
-    private int stopBits; // 1
+    private int stopBits = 1;
 
-    private int parity; // NO_PARITY
+    private int parity = SerialPort.NO_PARITY;
 
 }
