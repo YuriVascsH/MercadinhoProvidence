1b0a400f6d2a449ec8dd60f757777cedf9c2b865
Felipe Sabino de Oliveira Lima <fsabino006@gmail.com>
2026-07-21 09:20:49 -0300

    refactor: improved interface of BalanceProtocol

--- mercadinho-providence/src/main/java/br/com/mercadinhoprovidence/balance/protocol/BalanceProtocol.java
+++ mercadinho-providence/src/main/java/br/com/mercadinhoprovidence/balance/protocol/BalanceProtocol.java
@@ -1,7 +1,17 @@
 package br.com.mercadinhoprovidence.balance.protocol;
 
+package br.com.mercadinhoprovidence.balanca.protocol;
+
+import br.com.mercadinhoprovidence.balanca.Weight;
+
 public interface BalanceProtocol {
 
-    Peso interpretar(String resposta);
+    /**
+     * Interpreta a resposta recebida da balança e converte em um objeto Weight.
+     *
+     * @param response resposta bruta recebida da porta serial
+     * @return objeto Weight contendo o valor e o estado do peso
+     */
+    Weight parse(String response);
 
 }
