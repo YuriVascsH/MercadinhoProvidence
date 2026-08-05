0bd40068cb7931f2bf268811043710c9b7efceca
Felipe Sabino de Oliveira Lima <fsabino006@gmail.com>
2026-08-05 19:29:35 -0300

    fix: method name

--- mercadinho-providence/src/main/java/br/com/mercadinhoprovidence/balance/protocol/BalanceProtocol.java
+++ mercadinho-providence/src/main/java/br/com/mercadinhoprovidence/balance/protocol/BalanceProtocol.java
@@ -1,5 +1,7 @@
 package br.com.mercadinhoprovidence.balance.protocol;
 
+import br.com.mercadinhoprovidence.balance.Weight;
+
 public interface BalanceProtocol {
 
     Weight parse(String response);
