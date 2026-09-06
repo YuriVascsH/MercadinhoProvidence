3e6115fb5df039ebdb0b2c9b3c56cf27094d5397
Felipe Sabino de Oliveira Lima <fsabino006@gmail.com>
2026-09-06 17:41:03 -0300

    fix: class name

---
+++ mercadinho-providence/src/main/java/br/com/mercadinhoprovidence/dto/produto/ProductFormDto.java
@@ -0,0 +1,25 @@
+package br.com.mercadinhoprovidence.dto.produto;
+
+import java.math.BigDecimal;
+
+import br.com.mercadinhoprovidence.model.enums.Category;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class ProductFormDto {
+    
+    private Integer id;
+    public String nome;
+    public String codigoBarras;
+    public String descricao;
+    public Category categoria;
+    public boolean controlaEstoque;
+    public BigDecimal precoUnitario;
+    public BigDecimal precoPorKg;
+}
