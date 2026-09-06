a4b22b1ee594ef528c7450a516f5db7299c4dd6f
Felipe Sabino de Oliveira Lima <fsabino006@gmail.com>
2026-09-06 17:35:46 -0300

    refactor: removida a redundância da presença de ID na classe (apesar de sua existência, o campo era chamado em funções de classes externas)

--- mercadinho-providence/src/main/java/br/com/mercadinhoprovidence/dto/produto/ProductUpdateDto.java
+++ mercadinho-providence/src/main/java/br/com/mercadinhoprovidence/dto/produto/ProductUpdateDto.java
@@ -15,7 +15,6 @@ import lombok.Setter;
 @AllArgsConstructor
 public class ProductUpdateDto {
 
-    private Integer id;
     private String nome;
     private String codigoBarras;
     private String descricao;
