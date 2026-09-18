e6ed26c0bdf147121606924f1455440314da06ec
Felipe Sabino de Oliveira Lima <fsabino006@gmail.com>
2026-09-18 10:34:59 -0300

    feat: classes improved

--- mercadinho-providence/src/main/java/br/com/mercadinhoprovidence/balance/BalanceRamuza.java
+++ mercadinho-providence/src/main/java/br/com/mercadinhoprovidence/balance/BalanceRamuza.java
@@ -1,5 +1,42 @@
 package br.com.mercadinhoprovidence.balance;
 
-public class BalanceRamuza {
-    
+import br.com.mercadinhoprovidence.balance.config.BalanceConfig;
+import br.com.mercadinhoprovidence.balance.protocol.BalanceProtocol;
+import br.com.mercadinhoprovidence.balance.serial.SerialConnection;
+
+public class BalanceRamuza implements Balance {
+
+    private final SerialConnection serialConnection;
+    private final BalanceReader reader;
+
+    public BalanceRamuza(
+            BalanceConfig config,
+            BalanceProtocol protocol) {
+
+        this.serialConnection = new SerialConnection(config);
+        this.reader = new BalanceReader(
+                serialConnection,
+                protocol
+        );
+    }
+
+    @Override
+    public void connect() {
+        serialConnection.connect();
+    }
+
+    @Override
+    public void disconnect() {
+        serialConnection.disconnect();
+    }
+
+    @Override
+    public boolean isConnected() {
+        return serialConnection.isConnected();
+    }
+
+    @Override
+    public Weight readWeight() {
+        return reader.readWeight();
+    }
 }
